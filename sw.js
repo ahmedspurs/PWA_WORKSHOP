99d6edac537cbeaa6c42ee48333ca2e38d795f8a
Ahmed adil <57252782+ahmedspurs@users.noreply.github.com>
2021-03-19 21:56:43 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const staticDevCoffee = "PWA workShop"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/1.jpg",
+  "/images/2.jpg",
+  "/images/3.jpg",
+  "/images/4.jpg",
+  "/images/5.jpg",
+  "/images/6.jpg",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
